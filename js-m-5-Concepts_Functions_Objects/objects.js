89ba60097d24b1ddb83cb9524da5af269c30ffb3
Oyas0 <oyasurrahman0@gmail.com>
2022-08-04 02:00:03 +0600

    test

--- js-m-5-Concepts_Functions_Objects/objects.js
+++ js-m-5-Concepts_Functions_Objects/objects.js
@@ -1,12 +1,19 @@
 var student = { id:110, 
     name:"solaiman khan",
     class:10,
-    marks:80+
+    marks:80
 }
 var mobile = {
     bramd:'samsung',
-    price = 20000,
+    price: 20000,
     stroge: '64gb',
     camara: '12mp',
     color: 'black'
+}
+var computer = {
+    brand: 'lenovo',
+    price: 64000,
+    stroge: '100GB',
+    color: 'OffWhite',
+
 }
