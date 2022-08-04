5098d263f5eb224604473a4eb0749a778ef9ea4a
Oyas0 <oyasurrahman0@gmail.com>
2022-08-05 05:09:39 +0600

    leap year

--- js-m-6-Apply_javascript_concepts/odd-or-even.js
+++ js-m-6-Apply_javascript_concepts/odd-or-even.js
@@ -18,39 +18,81 @@
 // console.log(3321%2);
 // console.log(35%2);
 
+/** is that number is odd ?? or even?? */
 
-function isEven(number) {
-    const remainder = number % 2;
-    // console.log(remainder);
-    if (remainder == 0) {
-    console.log('number is even');      
+// function isEven(number) {
+//     const remainder = number % 2;
+//     if (remainder == 0) {
+//         console.log('number is odd');
+//     } else {
+//         console.log('number is even');
+//     }
+// }
+
+// isEven(302);
+// isEven(303);
+// isEven(304);
+// isEven(304);
+// isEven(305);
+// isEven(306);
+
+
+
+// function isEven(number) {
+//     const remainder = number % 2;
+//     // console.log(remainder);
+//     if (remainder == 0) {
+//     return true;     
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// const myNumber = isEven(305);
+// console.log( myNumber);
+// const herNumber = isEven(400);
+// console.log(herNumber);
+
+        /** is the year is leap year */
+        /**  program to check leap year */
+function isLeapYear(number) {
+    const remainders = number % 4;
+    const remainders1 = number % 100;
+    const remainders2 = number % 400;
+    // console.log(remainders);
+    if ((remainders == 0) && (remainders1 != 0) || (remainders2 == 0)) {
+    console.log('the year is leap year');      
     }
     else{
-        console.log('number is odd');
+        console.log('The  year is not leap year');
     }
 }
+// isLeapYear(2);
+isLeapYear(2021);
+isLeapYear(2024);
+isLeapYear(1900);
+isLeapYear(2022);
 
-isEven(303);
-isEven(302);
-isEven(301);
-isEven(304);
-isEven(305);
-isEven(306);
+// isLeapYear(2);
 
 
 
-function isEven(number) {
-    const remainder = number % 2;
-    // console.log(remainder);
-    if (remainder == 0) {
-    return true;     
-    }
-    else{
-        return false;
-    }
-}
+    
+// function checkLeapYear(year) {
+//     if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+//         console.log(year + ' is a leap year');
+//     } else {
+//         console.log(year + ' is not a leap year');
+//     }
+// }
 
-const myNumber = isEven(305);
-console.log( myNumber);
-const herNumber = isEven(400);
-console.log(herNumber);
+// // take input
+// const theYear = 2002;
+// const isThatLeapYear = checkLeapYear(theYear);
+// console.log(isThatLeapYear);
+
+
+// const theYear1 = 1900;
+// const isThatLeapYear1 = checkLeapYear(theYear1);
+// console.log(isThatLeapYear1);
