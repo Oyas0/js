79c69aac79a7dac823ecbc59cb3720e4fd10545b
Oyas0 <oyasurrahman0@gmail.com>
2022-08-05 04:42:32 +0600

    frist commit

--- js-m-6-Apply_javascript_concepts/odd-or-even.js
+++ js-m-6-Apply_javascript_concepts/odd-or-even.js
@@ -11,9 +11,46 @@
 // console.log(17/2);
 // console.log(19/2);
 
-/** vagshesh dekha */
-console.log(34%2);
-console.log(34123%2);
-console.log(312%2);
-console.log(3321%2);
-console.log(35%2);
+// /** vagshesh dekha */
+// console.log(34%2);
+// console.log(34123%2);
+// console.log(312%2);
+// console.log(3321%2);
+// console.log(35%2);
+
+
+function isEven(number) {
+    const remainder = number % 2;
+    // console.log(remainder);
+    if (remainder == 0) {
+    console.log('number is even');      
+    }
+    else{
+        console.log('number is odd');
+    }
+}
+
+isEven(303);
+isEven(302);
+isEven(301);
+isEven(304);
+isEven(305);
+isEven(306);
+
+
+
+function isEven(number) {
+    const remainder = number % 2;
+    // console.log(remainder);
+    if (remainder == 0) {
+    return true;     
+    }
+    else{
+        return false;
+    }
+}
+
+const myNumber = isEven(305);
+console.log( myNumber);
+const herNumber = isEven(400);
+console.log(herNumber);
