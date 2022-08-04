87c319849446deae015890ded45d6010d47d06a0
Oyas0 <oyasurrahman0@gmail.com>
2022-08-05 05:30:25 +0600

    find odd sum

---
+++ js-m-6-Apply_javascript_concepts/findoddsum.js
@@ -0,0 +1,31 @@
+// 1.Array niye er man gula jog korar fucntion.
+
+// function getSumOfArray(numbers) {
+//     console.log(numbers);
+//     let sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         const index = i ;
+//         const elements = numbers[index]
+//         sum = sum + elements;
+//         console.log(index, elements,sum );
+//     }
+//     return sum;
+// }
+
+// const myNumbers = [12,23,34,45,65,67,78,84,549]
+// getSumOfArray(myNumbers);
+
+function jogKoroReVAu(tumarJaLage) {
+    console.log(jogKoroReVAu);
+    let sum = 0;
+    for (let i = 0; i < tumarJaLage.length; i++) {
+        const nidex = i ;
+        const element = tumarJaLage[nidex];
+        sum = sum + element;
+        console.log(nidex, element, sum);
+ 
+    } 
+    return sum; 
+}
+const amrNumber = [12,23,34,45,54,5667,3,23,12,32]
+jogKoroReVAu(amrNumber);
