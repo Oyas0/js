09e2f5807a659fe598e24ee4377d0248d0332c8e
Oyas0 <oyasurrahman0@gmail.com>
2022-08-05 06:37:19 +0600

    asdf

--- js-m-6-Apply_javascript_concepts/findoddsum.js
+++ js-m-6-Apply_javascript_concepts/findoddsum.js
@@ -15,17 +15,86 @@
 // const myNumbers = [12,23,34,45,65,67,78,84,549]
 // getSumOfArray(myNumbers);
 
-function jogKoroReVAu(tumarJaLage) {
-    console.log(jogKoroReVAu);
-    let sum = 0;
-    for (let i = 0; i < tumarJaLage.length; i++) {
-        const nidex = i ;
-        const element = tumarJaLage[nidex];
-        sum = sum + element;
-        console.log(nidex, element, sum);
+// function jogKoroReVAu(tumarJaLage) {
+//     console.log(jogKoroReVAu);
+//     let sum = 0;
+//     for (let i = 0; i < tumarJaLage.length; i++) {
+//         const nidex = i ;
+//         const element = tumarJaLage[nidex];
+//         sum = sum + element;
+//         console.log(nidex, element, sum);
  
-    } 
-    return sum; 
+//     } 
+//     return sum; 
+// }
+// const amrNumber = [12,23,34,45,54,5667,3,23,12,32]
+// jogKoroReVAu(amrNumber);
+
+
+
+// function getOddNumberAnArray(numbers) {
+//     const oddnumbers  = []
+//     for (let i = 0; i < numbers.length; i++) {
+//         const index = i;
+//         const element = numbers[index];
+//         if (element % 2 !== 0) {
+//             console.log(index, element);
+//             oddnumbers.push(element);
+//         }
+        
+//     }
+//     return oddnumbers;
+    
+// }
+
+// const amr1Number = [12,23,34,45,54,5667,3,23,23,32,43,543,454323,3243,412,12,32];
+// const oddNUmbers = getOddNumberAnArray(amr1Number);
+// console.log(oddNUmbers);
+
+
+
+
+
+
+    // function getOddNumberAnArray(numbers) {
+    //     const oddnumbers  = []
+    //     for (let i = 0; i < numbers.length; i++) {
+    //         const index = i;
+    //         const element = numbers[index];
+    //         if (element % 2 !== 0) {
+    //             console.log(index, element);
+    //             oddnumbers.push(element);
+    //         }
+            
+    //     }
+    //     return oddnumbers;
+        
+    // }
+    
+    // const amr1Number = [12,23,34,45,54,5667,3,23,23,32,43,543,454323,3243,412,12,32];
+    // const oddNUmbers = getOddNumberAnArray(amr1Number);
+    // console.log(oddNUmbers);
+    
+// }
+
+
+
+
+
+// 1+2+3+4+5+6+7+8+9=??
+// let sum = 0;
+// for (let i = 0; i <= 8; i++) {
+//     sum = sum + i;
+//     console.log(i, sum);
+    
+// }
+
+
+function sumofNumber(number){
+    let sum = 0;
+    for (let i = 0; i <= 8; i++) {
+        sum = sum + 1;
+        console.log(i,sum);
+    }
+    return sum;
 }
-const amrNumber = [12,23,34,45,54,5667,3,23,12,32]
-jogKoroReVAu(amrNumber);
