5f6c37725cf0ae414e6f62f1b81688bc7a3088d1
Oyas0 <oyasurrahman0@gmail.com>
2022-08-07 04:04:47 +0600

    msadf

---
+++ js-m-8 Simple problem solving/math.js
@@ -0,0 +1,31 @@
+// const result = Math.pow(3, 8);
+// console.log(result);
+
+
+// const num1 = 25;
+// const num2 = 45;
+
+// const gap = Math.abs(num1 - num2);
+// console.log(gap);
+// if (gap < 5) {
+//     console.log('Tora bondho hoite parbe');
+// } else {
+//     console.log('torar bondho howyar dorkar nai');
+// }
+
+// const number = 2.54234;
+// const fullNumber = Math.ceil(2.0000001);
+// console.log(fullNumber);
+
+
+// random
+
+// const random = Math.random()*100;
+// console.log(random);
+
+for (let i = 0; i < 20; i++) {
+    const random =Math.round(Math.random()*6);
+    console.log(random);
+
+    
+}
