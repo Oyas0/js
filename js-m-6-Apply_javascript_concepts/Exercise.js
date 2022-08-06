4634f3c3af2857025d55b405dcd10d4c33de2e7d
Oyas0 <oyasurrahman0@gmail.com>
2022-08-07 04:08:39 +0600

    dhsagfdas

---
+++ js-m-6-Apply_javascript_concepts/Exercise.js
@@ -0,0 +1,74 @@
+/** EX 2 */
+// find largest number 
+// has no return
+// function findLargest(num1,num2,num3) {
+    
+//     if (num1 > num2 && num1 > num3) {
+//         console.log(num1);
+//     } else if(num2 > num3 && num2 > num3) {
+//         console.log(num2);
+//     }else{
+//         console.log(num3);
+//     }
+// }
+// findLargest(2,3,1);
+// findLargest(5,8,1);
+// findLargest(5,8,10);
+
+// return
+
+// function findLargestNumber(num1,num2,num3) {
+    
+//     if (num1 > num2 && num1 > num3) {
+//         console.log(num1);
+//     } else if(num2 > num3 && num2 > num3) {
+//         console.log(num2);
+//     }else{
+//         return(num3)
+//     }
+// }
+
+
+// console.log(findLargestNumber(4,67,48));
+// console.log(findLargestNumber(12124,627,248));
+// console.log(findLargestNumber(42321,63247,4821));
+
+
+
+/** let const */
+//  man change kora jai
+// man change korar proyojon thakle let use korte hoi
+let age = 18;
+console.log(age);
+
+age = 19;
+console.log(age);
+age = 23;
+console.log(age);
+age = 49;
+console.log(age);
+
+
+
+// const
+// man change kora jai nah 
+// man change korar proyojon nah thakle const use korte hoi
+// const x = 32;
+// console.log(x);
+
+// x = 33,
+// console.log(x);
+
+
+if (true) {
+    var x = 50;
+}
+if (true) {
+    var y = 60;
+}
+console.log("var vs let:");
+console.log(x);
+console.log(y);
+
+
+/** objects */
--- js-m-6-Apply_javascript_concepts/multiNumber.js
+++ js-m-6-Apply_javascript_concepts/multiNumber.js
@@ -1,12 +1,12 @@
 // 1*2*3*4*5*6*7*8*9
 function reFactorial(number) {
-    let result = 8;
-    for (let i = 0; i => number; i++) {
+    let result = 1;
+    for (let i = 1; i => number; i++) {
        result = result * i;
         
     }  
     return result;  
 }
 
-const result = reFactorial(8);
+const result = reFactorial(9);
 console.log(result);
